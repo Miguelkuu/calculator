c83d593f7e4437b3c7de25db0ba142840c72378a
miguel <migueljpb7@gmail.com>
2022-03-03 17:25:55 -0500

    update on tkinter calculator

--- tk_calculator.py
+++ tk_calculator.py
@@ -1,27 +1,125 @@
-import tkinter as ttk
+from tkinter import *
+
 
 root = Tk()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
+root.geometry("208x300")
+root.resizable(False, False)
 
-ttk.Button(frm, text="AC").grid(column=0, row=0)
-ttk.Button(frm, text="+/-").grid(column=1, row=0)
-ttk.Button(frm, text="%").grid(column=2, row=0)
-ttk.Button(frm, text="/").grid(column=3, row=0)
-ttk.Button(frm, text="7").grid(column=0, row=1)
-ttk.Button(frm, text="8").grid(column=1, row=1)
-ttk.Button(frm, text="9").grid(column=2, row=1)
-ttk.Button(frm, text="*").grid(column=3, row=1)
-ttk.Button(frm, text="4").grid(column=0, row=2)
-ttk.Button(frm, text="5").grid(column=1, row=2)
-ttk.Button(frm, text="6").grid(column=2, row=2)
-ttk.Button(frm, text="-").grid(column=3, row=2)
-ttk.Button(frm, text="1").grid(column=0, row=3)
-ttk.Button(frm, text="2").grid(column=1, row=3)
-ttk.Button(frm, text="3").grid(column=2, row=3)
-ttk.Button(frm, text="+").grid(column=3, row=3)
-ttk.Button(frm, text="0").grid(row=4, column=0, columnspan=2, sticky="we")
-ttk.Button(frm, text=".").grid(column=2, row=4)
-ttk.Button(frm, text="=").grid(column=3, row=4)
+class aplication:
+    def __init__(self):
+        self.create_buttons()
+        self.a = ""
+        self.num1 = 0
+        self.num2 = 0
+        self.ope = ""
 
+    def create_buttons(self):
+        self.zero_button = Button(root, text="0", height=2, width=6)
+        self.zero_button.grid(column=0, row=6, columnspan=2, sticky="we")
+        self.zero_button["command"] = self.add_0_to_str
+
+        self.dot_button = Button(root, text=".", height=2, width=6)
+        self.dot_button.grid(column=2, row=6)
+        self.dot_button["command"] = self.add_dot_to_str
+
+        self.equal_button = Button(root, text="=", height=2, width=6)
+        self.equal_button.grid(column=3, row=6)
+
+
+        self.one_button = Button(root, text="1", height=2, width=6)
+        self.one_button.grid(row=5, column=0)
+        self.one_button["command"] = self.add_1_to_str
+
+        self.two_button = Button(root, text="2", height=2, width=6)
+        self.two_button.grid(row=5,column=1)
+        self.two_button["command"] = self.add_2_to_str
+
+        self.three_button = Button(root, text="3", height=2, width=6)
+        self.three_button.grid(row=5, column=2)
+        self.three_button["command"] = self.add_3_to_str
+
+        self.plus_button = Button(root, text="+", height=2,width=6)
+        self.plus_button.grid(row=5, column=3)
+
+        self.four_button = Button(root, text="4", height=2, width=6)
+        self.four_button.grid(row=4, column=0)
+        self.four_button["command"] = self.add_4_to_str
+
+        self.five_button = Button(root, text="5", height=2, width=6)
+        self.five_button.grid(row=4, column=1)
+        self.five_button["command"] = self.add_5_to_str
+
+        self.six_button = Button(root, text="6", height=2, width=6)
+        self.six_button.grid(row=4, column=2)
+        self.six_button["command"] = self.add_6_to_str
+
+        self.minus_button = Button(root, text="-", height=2, width=6)
+        self.minus_button.grid(row=4, column=3)
+
+        self.seven_button = Button(root, text="7", height=2, width=6)
+        self.seven_button.grid(row=3, column=0)
+        self.seven_button["command"] = self.add_7_to_str
+
+        self.eigth_button = Button(root, text="8", height=2, width=6)
+        self.eigth_button.grid(row=3, column=1)
+        self.eigth_button["command"] = self.add_8_to_str
+
+        self.nine_button = Button(root, text="9", height=2, width=6)
+        self.nine_button.grid(row=3, column=2)
+        self.nine_button["command"] = self.add_9_to_str
+
+        self.multiply_button = Button(root, text="*", height=2, width=6)
+        self.multiply_button.grid(row=3, column=3)
+
+        self.clear_button = Button(root, text="AC",height=2, width=6)
+        self.clear_button.grid(column=0, row=2, columnspan=2, sticky="we")
+        self.clear_button["command"] = self.clear_all
+
+        self.negative_button = Button(root, text="-/+", height=2, width=6)
+        self.negative_button.grid(column=2, row=2)
+
+
+        self.divide_button = Button(root, text="/", height=2, width=6, bg="orange", fg="white")
+        self.divide_button.grid(column=3, row=2)
+
+    def add_1_to_str(self):
+        self.a = self.a + "1"
+
+    def add_2_to_str(self):
+        self.a = self.a + "2"
+
+    def add_3_to_str(self):
+        self.a = self.a + "3"
+
+    def add_4_to_str(self):
+        self.a = self.a + "4"
+
+    def add_5_to_str(self):
+        self.a = self.a + "5"
+
+    def add_6_to_str(self):
+        self.a = self.a + "6"
+
+    def add_7_to_str(self):
+        self.a = self.a + "7"
+
+    def add_8_to_str(self):
+        self.a = self.a + "8"
+
+    def add_9_to_str(self):
+        self.a = self.a + "9"
+
+    def add_0_to_str(self):
+        self.a = self.a + "0"
+
+    def add_dot_to_str(self):
+        self.a = self.a + "."
+
+    def clear_all(self):
+        self.a = ""
+        self.num1 = 0
+        self.num2 = 0
+        self.ope = ""
+
+aplication()
 root.mainloop()
